98d5580684c5211b3f29b999c53c74004982f252
MdKawsarAK <kawsarhossain830@gmail.com>
2025-10-22 09:23:22 +0600

    php first

---
+++ test/index.php
@@ -0,0 +1,19 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to php</h1>
+    <?php
+    echo "hello world";
+    print "hello php";
+    ?>
+
+<?php ?>
+</body>
+</html>
+<?php ?>

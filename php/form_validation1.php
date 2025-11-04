115486c72de9d110ce71b61ea2117fa0b038f37f
MdKawsarAK <kawsarhossain830@gmail.com>
2025-11-04 13:10:35 +0600

    4 nov

---
+++ php/form_validation1.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        table,
+        td {
+            border: 1px solid #000;
+            border-collapse: collapse;
+        }
+    </style>
+</head>
+
+<body>
+
+
+
+
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+        Your Name:
+        <input type="text" name="fname" value="<?php if(isset($_POST['fname'])){ echo $_POST['fname'];} ?>"> <br> <br> <br>
+        email Address:
+        <input type="text" name="email"> <br> <br> <br>
+        Favorite programming Language:
+        <select name="language[]" multiple="multiple">
+            <option value="C#">C#</option>
+            <option value="Java">JAVA</option>
+            <option value="perl">Perl</option>
+            <option value="PHP">PHP</option>
+
+        </select> <br> <br><br>
+        Want to lern skills:
+        <input type="checkbox" name="skills[]" value="C">C
+        <input type="checkbox" name="skills[]" value="MySquel">MySquel
+        <input type="checkbox" name="skills[]" value="Jquery">Jquery <br> <br> <br>
+        <input type="submit" value="SUBMIT" name="submit">
+
+
+
+    </form>
+
+    <?php
+    if (isset($_POST["submit"])) {
+        $lang_output = "";
+        $skills_output = "";
+
+
+
+
+        $errors = array();
+
+        if (!isset($_POST['fname']) || $_POST['fname'] == "") {
+            $errors[] = "You must Enter Name";
+        } else {
+            $name = $_POST['fname'];
+        }
+
+        if (!isset($_POST['email']) || $_POST['email'] == "") {
+            $errors[] = "You must Enter Email";
+        } else {
+            $email = $_POST['email'];
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errors[] = "Email is not Valid";
+            }
+        }
+
+        if (!isset($_POST['language'])) {
+
+            $errors[] = "You must select one Language";
+        } else {
+            $lang = $_POST['language'];
+            foreach ($lang as $lan) {
+                $lang_output .= $lan . ",";
+            }
+        }
+        //array
+        //array
+
+        if (!isset($_POST['skills'])) {
+            $errors[] = "You must select one skill";
+        } else {
+            $skills = $_POST['skills'];
+            $lastskill = array_key_last($skills);  //last comma bad
+
+            foreach ($skills as $key => $skill) {
+                if ($key == $lastskill) {
+                    $skills_output .= $skill;
+                } else {
+
+                    $skills_output .= $skill . ",";
+                }
+            }
+        }
+
+        if (count($errors) > 0) {
+
+            echo "<ul>";
+            foreach ($errors as $error) {
+                echo "<li>" . $error . "</li>";
+            }
+            echo "</ul>";
+        } else {  ?>
+
+
+<?php
+
+?>
+            <table>
+
+                <tr>
+                    <td>Name:</td>
+                    <td><?php echo $name; ?></td>
+                </tr>
+                <tr>
+                    <td>Email:</td>
+                    <td><?php echo $email; ?></td>
+                </tr>
+                <tr>
+                    <td>Languages:</td>
+                    <td><?php echo $lang_output; ?></td>
+                </tr>
+                <tr>
+                    <td>Skills:</td>
+                    <td><?php echo $skills_output; ?></td>
+                </tr>
+            </table>
+
+
+
+
+    <?php
+        }
+    }
+
+    ?>
+
+
+</body>
+
+</html>

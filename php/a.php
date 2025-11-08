3cbe659d83033169c726584d5dc1dcd274586e7c
MdKawsarAK <kawsarhossain830@gmail.com>
2025-11-08 12:41:19 +0600

    8 nov

---
+++ php/a.php
@@ -0,0 +1,43 @@
+<?php 
+$a = "1";
+switch($a)
+{
+    case 1:
+        echo "hasan";
+        case 2: 
+            echo "rabbi";
+        
+        default:
+        echo "it is a men";
+        }
+
+
+?>
+<hr>
+<?php 
+$a  = "2";
+switch( print $a)
+{
+    case 2:
+        print "hasan";
+        break;
+        case 1: 
+           print  "rabbi";
+        break;
+        default:
+        print  "it is a men";
+        }
+
+
+?>
+<hr>
+
+<?php
+
+$n = 10;
+
+echo "what\n is $n";
+
+?>
+
+<hr>
---
+++ php/date.php
@@ -0,0 +1,7 @@
+<?php
+
+echo  date("Y-M-D h-i-s");
+date_default_timezone_set("Asia/Dhaka");
+echo "<br>";
+echo  date("Y-M-D h-i-s");
+?>
